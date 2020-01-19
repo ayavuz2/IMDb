e8773c178ef319a345bfab962fd31b9837893ec0
Ahmet Yavuz <a.yavuzum@gmail.com>
2020-01-19 20:39:37 +0300

    Test

--- imdb.py
+++ imdb.py
@@ -1,2 +1,18 @@
-print("Hello")
-print("Test")
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("https://www.imdb.com/chart/top/")
+
+soup = BeautifulSoup(r.content, "html.parser")
+
+var = soup.find_all("tbody", {"class":"lister-list"})
+
+var = var[0].find_all("tr")
+
+print("----------- TOP 250 -----------")
+
+for film in var:
+    filmbasliklari = (film.find_all("td", {"class":"titleColumn"}))
+    film_ismi = filmbasliklari[0].text
+    film_ismi = film_ismi.replace("\n","")
+    print(film_ismi)
