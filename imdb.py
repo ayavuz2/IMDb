8e1fc24023c28144a2ff7478501dc40bcdadf32d
Ahmet Yavuz <a.yavuzum@gmail.com>
2020-01-22 02:18:42 +0300

    Test

--- imdb.py
+++ imdb.py
@@ -1,5 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+import  _sqlite3
+#import  re
+
+def createTable():
+    con.execute("CREATE TABLE IF NOT EXISTS Top250(Title TEXT, ReleaseDate INT, Rating REAL)")
+
+#def addValue():
+
+
+con = _sqlite3.connect("imdb.db")
+cursor = con.cursor()
 
 r = requests.get("https://www.imdb.com/chart/top/")
 
@@ -9,10 +20,24 @@ var = soup.find_all("tbody", {"class":"lister-list"})
 
 var = var[0].find_all("tr")
 
-print("----------- TOP 250 -----------")
+film_arr = []
+
+#print("----------- TOP 250 -----------")
 
 for film in var:
     filmbasliklari = (film.find_all("td", {"class":"titleColumn"}))
     film_ismi = filmbasliklari[0].text
     film_ismi = film_ismi.replace("\n","")
-    print(film_ismi)
+
+    #print (re.findall(r"[\w']+", film_ismi))
+
+    #film_arr = film_ismi.split(".      ")
+    #txt = film_arr[1]
+    #print(int(txt[txt.find('(')+1 : txt.find(')')])) # Filmin cikis tarihini cekiyor
+    #print((txt.split("("))[0])
+
+
+    #deger = (film.find_all("td", {"class": "ratingColumn imdbRating"}))
+    #rating = deger[0].text
+    #rating = rating.replace("\n","")
+
